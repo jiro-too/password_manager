3398915a8006f3bacc750a3b2bfcd822ae0ba85d
Soham Nandy <soham.nandy@outlook.com>
2022-12-08 10:38:09 +0530

    Removed unnecessarry files

--- src_p/setup.py
+++ src_p/setup.py
@@ -3,7 +3,7 @@ def maths_setup():
     setup(name="maths",
           include_dirs= ["./src_c/"],
           version="1.0.0",
-          description="Python interface for the fputs C library function",
+          description="Python interface for the sqrt C library function",
           author="Soham Nandy",
           author_email="soham.nandy2006@gmail.com",
           ext_modules=[Extension("maths", ["./src_c/mathsmodule.c"])]),
@@ -13,8 +13,7 @@ def crypt_setup():
       setup(name="hashers",
           include_dirs= ["./src_c/"],
           version="1.0.0",
-          description="C implementation of SHA256 algorithm, sourced from \
-https://github.com/amosnier/sha-2",
+          description="C implementation of MD5 algorithm",
           author="Soham Nandy",
           author_email="soham.nandy2006@gmail.com",
           ext_modules=[Extension("hashers", ["./src_c/md5sum.c"])]),
